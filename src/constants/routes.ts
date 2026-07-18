5d74c31ec096e4c86a733b61cc5000a896ba0665
85Chong <68360937+YOOCHUNGKEUN@users.noreply.github.com>
2026-07-19 04:13:21 +0900

    * feat : 식수대장 링크 수정

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,5 @@
 export const LINKS = {
   velog: 'https://velog.io/@tosoon3535/series/Portfolio',
-  siksu: 'https://siksudaejang.co.kr',
+  siksu: 'https://admin.siksudaejang.co.kr/admin/login',
   email: 'mailto:tosoon3535@gmail.com',
 } as const;
